0bec01280e8266eefbc0151040276b235efd202d
SandeepKumar377 <sandeepkumar.sk5.ss55@mail.com>
2022-10-11 23:33:36 +0530

    Ng-switch-directive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  switchTest:number=3;
+
   myClass={'success':true, 'height24':false,'fontSize':this.increeseFont()};
   src='https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80';
   width=200;
